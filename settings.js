60745f90fd14f29dbe2676a1b493912ee5d07806
abdhulgithub <111677291+abdhulgithub@users.noreply.github.com>
2022-08-25 09:01:52 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,8 +54,8 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['94774579505'] //ur owner number
-global.ownername = "🎸 KAVEESHA " //ur owner name
+global.vcardowner = ['94763867158'] //ur owner number
+global.ownername = "❰❰⦁ 𝛫𝛯𝘕𝘡𝘖 ⦁❱❱ " //ur owner name
 global.ytname = "YT: Nero" //ur yt chanel name
 global.socialm = "GitHub: Nero" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
